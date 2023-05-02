f85056811526fe49eeb9a770c974ff2933ff40a6
Jefferson Quintero <jqb2409@gmail.com>
2023-05-02 14:33:54 -0500

    Primera clase seccion-3

---
+++ src/main/java/com/bolsadeideasspringboot/di/app/models/service/MiServicio.java
@@ -0,0 +1,7 @@
+package com.bolsadeideasspringboot.di.app.models.service;
+
+public class MiServicio {
+    public String operacion() {
+        return "ejecutando algún proceso importante";
+    }
+}
